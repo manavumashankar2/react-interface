d254b1006becb9d5bd62b7fb3d29c3b0331019ce
manav k <manav.k@metricstream.com>
2021-02-11 02:07:06 +0530

    Committing the react code

---
+++ src/components/App.js
@@ -0,0 +1,162 @@
+import '../css/App.css';
+import React , {Component, useState} from 'react' ; 
+
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments' ; 
+import {findIndex ,  without} from 'lodash' ;
+
+
+class App extends  Component{
+
+  constructor(){
+    super();
+    this.state = {
+      myAppointments :[],
+      formDisplay : false,
+      orderBy : 'petName',
+      orderDir : 'asc',
+      lastIndex: 0 ,
+      queryText : ''
+
+
+    }
+    this.deleteappointment =  this.deleteappointment.bind(this);
+    this.toggleForm  = this.toggleForm.bind(this) ; 
+    this.addAppointment  = this.addAppointment.bind(this) ; 
+    this.changeOrder  = this.changeOrder.bind(this) ; 
+    this.searchApts  = this.searchApts.bind(this) ; 
+    this.updateInfo  = this.updateInfo.bind(this) ; 
+
+    
+
+  }
+
+
+  updateInfo(name ,value , id){
+    let tempApts = this.state.myAppointments ; 
+    let aptIndex = findIndex(this.state.myAppointments , {
+      aptId  : id
+    });
+    tempApts[aptIndex][name] = value ;
+    this.setState({
+      myAppointments : tempApts
+    })
+  }
+
+  searchApts(val){
+    this.setState({
+      queryText : val
+    })
+  }
+
+  changeOrder(orderby , orderdir){
+    this.setState ({
+      orderBy : orderby ,
+      orderDir : orderdir
+    })
+    
+  }
+
+  addAppointment(apt){
+    let tempApts =  this.state.myAppointments ;
+    apt.aptId = this.state.lastIndex ; 
+    tempApts.unshift(apt) ; 
+    this.setState({
+      myAppointments :tempApts , 
+      lastIndex : this.state.lastIndex + 1
+    }) ; 
+  }
+
+  deleteappointment(apt){
+    let tempApts = this.state.myAppointments   ;
+    tempApts = without(tempApts,apt);
+    this.setState({
+      myAppointments :tempApts 
+    });
+  }
+
+  toggleForm(){
+    this.setState({
+      formDisplay : !this.state.formDisplay
+    });
+  }
+
+  componentDidMount(){
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(result => {
+      const apts = result.map(item => {
+        item.aptId = this.state.lastIndex ;
+        this.setState({lastIndex : this.state.lastIndex+1})
+        return item ;
+      });
+       
+    this.setState({
+      myAppointments: apts
+    });
+    });
+   
+
+  }
+
+  render(){
+   
+    let order ;
+    let filteredApts = this.state.myAppointments ; 
+    if(this.state.orderDir === 'asc'){
+      order = 1 ; 
+
+    }
+    else{
+      order = -1 ; 
+
+    }
+    filteredApts =  filteredApts
+    .sort((a,b) => {
+      if(a[this.state.orderBy].toLowerCase() < b[this.state.orderBy].toLowerCase() ){
+        return -1 * order ;
+      }
+      else{
+        return 1 * order ;
+      }
+    })
+    .filter(eachItem => {
+      return (
+        eachItem['petName']
+          .toLowerCase()
+          .includes(this.state.queryText.toLowerCase()) ||
+        eachItem['ownerName']
+          .toLowerCase()
+          .includes(this.state.queryText.toLowerCase()) ||
+        eachItem['aptNotes']
+          .toLowerCase()
+          .includes(this.state.queryText.toLowerCase()) 
+      );
+    });
+
+  return (
+    <main className="page bg-white" id="petratings">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 bg-white">
+          <div className="container">
+            <AddAppointments formDisplay = {this.state.formDisplay}
+            toggleForm = {this.toggleForm}
+            addAppointment = {this.addAppointment}/>
+            <SearchAppointments   orderBy = {this.state.orderBy } orderDir = {this.state.orderDir} 
+            changeOrder  = {this.changeOrder}
+            searchApts = {this.searchApts}/>
+            <ListAppointments appointments = {filteredApts }
+            deleteappointment = {this.deleteappointment} 
+            updateInfo = {this.updateInfo}/>
+          </div>  
+        </div>
+      </div>
+    </div>
+  </main>
+  );
+  }
+}
+
+export default App;
